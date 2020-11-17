e6da72c0d04b39de9bc46b8f7d792959aef2c10e
Andrey Proshkin <zx80live@gmail.com>
2020-11-17 13:09:43 +0300

    reduce equals

--- src/main/scala/com/zx80live/gofp/bootstrap/functions/FuncEquals.scala
+++ src/main/scala/com/zx80live/gofp/bootstrap/functions/FuncEquals.scala
@@ -9,36 +9,20 @@ object FuncEquals {
 
   def body(t: Type): String = t match {
     case _: OptionType =>
-      s"""
-         |  if a.IsDefined() {
-         |    if b.IsDefined() {
-         |      return ${name(t.underlined)}(*a.value, *b.value)
-         |    } else { return false }
-         |  } else if b.IsDefined() {
-         |    return false
-         |  } else { return true }
-         |""".stripMargin
+      s""" if a.IsDefined() { if b.IsDefined() { return ${name(t.underlined)}(*a.value, *b.value) } else { return false } } else if b.IsDefined() { return false } else { return true }"""
 
     case _: ArrayType =>
-      s"""
-         |  len1 := len(a)
-         |  if len1 != len(b) { return false }
-         |
-         |  for i := 0; i < len1; i ++ {
-         |    if !${name(t.underlined)}(a[i], b[i]) { return false }
-         |  }
-         |  return true
-         |""".stripMargin
+      s""" len1 := len(a); if len1 != len(b) { return false }; for i := 0; i < len1; i ++ { if !${name(t.underlined)}(a[i], b[i]) { return false } }; return true"""
+
+    case _: ListType =>
+      s""" if a.Size() != b.Size() { return false }; xs1 := a; xs2 := b; for xs1.NonEmpty() { if !${FuncEquals.name(t.underlined)}(*xs1.head, *xs2.head) { return false }; xs1 = *xs1.tail; xs2 = *xs2.tail }; return true"""
     case _ =>
-      s"""
-         |return a == b""".stripMargin
+      s""" return a == b"""
   }
 
   def func(t: Type): String =
     s"""
-       |${contract(t)} {
-       |  ${body(t)}
-       |}""".stripMargin
+       |${contract(t)} { ${body(t)} }""".stripMargin
 
   def functions: Seq[String] = (BaseType.types ++ OptionType.types ++ ArrayType.types ++ ListType.types).map(func)
 }
