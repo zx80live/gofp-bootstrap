1e5dacb3214cb9de70333aa6ee9421d19e88df69
Andrey Proshkin <zx80live@gmail.com>
2020-11-21 19:00:37 +0300

    refactoring

--- src/main/scala/com/zx80live/gofp/bootstrap/types/MonadType.scala
+++ src/main/scala/com/zx80live/gofp/bootstrap/types/MonadType.scala
@@ -16,17 +16,18 @@ trait MonadType extends TraversableType {
     s"""
        |func (m $raw) Map${out.view}(f func(${underlined.raw}) ${out.raw}) ${rawFrom(out)} { return ${FuncMap.name(this, out)}(m, f) }""".stripMargin
 
+  def flatMapDeclaration(out: MonadType): String = s"func (m $raw) FlatMap${out.view}(f func(${underlined.raw}) ${out.raw}) ${rawFrom(out.underlined)}"
 
   def funcFlatMap(out: MonadType): String = (this, out) match {
     case (_ : OptionType, o2: OptionType) =>
       s"""
-         |func (m $raw) FlatMap${out.view}(f func(${underlined.raw}) ${out.raw}) ${rawFrom(out.underlined)} { if m.IsDefined() { return f(*m.value) } else { return ${o2.noneName} } }""".stripMargin
+         |${flatMapDeclaration(out)} { if m.IsDefined() { return f(*m.value) } else { return ${o2.noneName} } }""".stripMargin
     case (_ : ListType, l2: ListType) =>
       s"""
-         |func (m $raw) FlatMap${out.view}(f func(${underlined.raw}) ${out.raw}) ${rawFrom(out.underlined)} { if m.IsEmpty() { return ${l2.nilName} } else { acc := ${l2.nilName}; xs := m; for xs.NonEmpty() { exs := f(*xs.head); for exs.NonEmpty() { acc = acc.Cons(*exs.head); exs = *exs.tail }; xs = *xs.tail }; return acc.Reverse() } }""".stripMargin
+         |${flatMapDeclaration(out)} { if m.IsEmpty() { return ${l2.nilName} } else { acc := ${l2.nilName}; xs := m; for xs.NonEmpty() { exs := f(*xs.head); for exs.NonEmpty() { acc = acc.Cons(*exs.head); exs = *exs.tail }; xs = *xs.tail }; return acc.Reverse() } }""".stripMargin
     case _ =>
       s"""
-         |func (m $raw) FlatMap${out.view}(f func(${underlined.raw}) ${out.raw}) ${rawFrom(out.underlined)} { panic("does not supported: $raw.FlatMap(${out.raw})") } }
+         |${flatMapDeclaration(out)} { panic("does not supported: $raw.FlatMap(${out.raw})") } }
          |""".stripMargin
   }
 
